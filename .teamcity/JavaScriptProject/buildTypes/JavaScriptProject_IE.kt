9aa37e9f77666d7c846ea24448b9875e37679774
Nikolay Kozhemyak <nkozhemyak@gmail.com>
2018-02-13 15:07:37 +0600

    Update JavaScriptProject_IE.kt

--- .teamcity/JavaScriptProject/buildTypes/JavaScriptProject_IE.kt
+++ .teamcity/JavaScriptProject/buildTypes/JavaScriptProject_IE.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.*
 
 object JavaScriptProject_IE : BuildType({
     template(JavaScriptProject.buildTypes.JavaScriptProject_Template)
-    uuid = "4e548859-2dea-4811-8f04-0addae46c0a4"
+    uuid = "JavaScriptProject_IE"
     id = "JavaScriptProject_IE"
     name = "02. Internet Explorer"
 
