923c75c089b263a2e0ac35438051995d2f84e89e
Nikolay Kozhemyak <nkozhemyak@gmail.com>
2018-02-13 15:03:55 +0600

    Update JavaScriptProject_IE.kt

--- .teamcity/JavaScriptProject/buildTypes/JavaScriptProject_IE.kt
+++ .teamcity/JavaScriptProject/buildTypes/JavaScriptProject_IE.kt
@@ -2,7 +2,7 @@ package JavaScriptProject.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 
-object JavaScriptProject_Chrome : BuildType({
+object JavaScriptProject_IE : BuildType({
     template(JavaScriptProject.buildTypes.JavaScriptProject_Template)
     uuid = "4e548859-2dea-4811-8f04-0addae46c0a4"
     id = "JavaScriptProject_IE"
