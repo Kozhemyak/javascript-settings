a9b3cb4224857981598a6e4a2ab21cbce9400905
devops <devops@TeamCity>
2018-02-13 11:13:55 +0000

    TeamCity change in 'JavaScript Project' project: runners of '03. Deploy To Stagging' build configuration were updated

--- .teamcity/JavaScriptProject/patches/buildTypes/f328c924-570c-4f34-999f-ac9be2a0c268.kts
+++ .teamcity/JavaScriptProject/patches/buildTypes/f328c924-570c-4f34-999f-ac9be2a0c268.kts
@@ -1,6 +1,7 @@
 package JavaScriptProject.patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2017_2.ui.*
 
 /*
@@ -9,6 +10,19 @@ To apply the patch, change the buildType with uuid = 'f328c924-570c-4f34-999f-ac
 accordingly and delete the patch script.
 */
 changeBuildType("f328c924-570c-4f34-999f-ac9be2a0c268") {
+    expectSteps {
+    }
+    steps {
+        insert(0) {
+            script {
+                scriptContent = """
+                    rmdir /S /Q c:\inetpub\wwwroot
+                    xcopy /S /I /Y app c:\inetpub\wwwroot\
+                """.trimIndent()
+            }
+        }
+    }
+
     dependencies {
         remove("JavaScriptProject_IE") {
             snapshot {
